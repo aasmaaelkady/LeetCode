0ae96f2c1bf4b4208054c8771ee592aaf1cd7244
Asmaa <aasmaaelkady@gmail.com>
2026-06-30 00:43:21 +0300

    Time: 4 ms (98.01%) | Memory: 55.7 MB (98.08%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,20 +1,15 @@
 function isAnagram(s: string, t: string): boolean {
     if(s.length !== t.length) return false;
-    
-    let sMap: Map<string, number> = new Map();
-    let tMap : Map<string, number> = new Map();
 
-    for(const sLetter of s){
-        sMap.set(sLetter, (sMap.get(sLetter) || 0)+1)
+    const arr: number[] = new Array(26).fill(0);
+
+    for(let i=0; i<s.length; i++){
+        arr[s.charCodeAt(i)-97]++
+        arr[t.charCodeAt(i)-97]--
     }
 
-     for(const tLetter of t){
-        tMap.set(tLetter, (tMap.get(tLetter) || 0)+1)
+    for(const i of arr){
+        if(arr[i] !== 0) return false;
     }
-
-    for(const [key,val] of sMap){
-        if(tMap.get(key) !== val) return false
-    }
-
-    return true
+    return true;
 };
