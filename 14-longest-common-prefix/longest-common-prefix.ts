5a345f0213f90b63ed3aead4a83f77eeb479b444
Asmaa <aasmaaelkady@gmail.com>
2026-07-01 03:06:17 +0300

    Time: 0 ms (100.00%) | Memory: 53.9 MB (97.92%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -0,0 +1,9 @@
+function longestCommonPrefix(strs: string[]): string {
+    let res: string = strs[0]
+    for(let i=1; i< strs.length; i++){
+        while(strs[i].indexOf(res)!== 0 && res !== ""){
+           res = res.substring(0,res.length-1)
+        }
+    }
+    return res
+};
