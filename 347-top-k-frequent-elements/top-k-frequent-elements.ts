8ba05c2c9923bbd6111b2a618dc28a9081476ca4
Asmaa <aasmaaelkady@gmail.com>
2026-07-09 13:20:59 +0300

    Time: 10 ms (72.94%) | Memory: 59.9 MB (76.99%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -0,0 +1,9 @@
+function topKFrequent(nums: number[], k: number): number[] {
+    const map : Map<number, number> = new Map();
+  
+    for(let num of nums){
+        map.set(num, (map.get(num)||0)+1)
+    }
+    
+    return [...map.entries()].sort((a,b)=> b[1] -a[1]).map((num)=> num[0]).slice(0,k)
+};
