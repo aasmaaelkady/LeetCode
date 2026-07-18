afd34dca5588ad46353c811574020d25114a9646
Asmaa <aasmaaelkady@gmail.com>
2026-07-18 10:34:45 +0300

    Time: 0 ms (100.00%) | Memory: 61.6 MB (77.36%) - LeetSync

---
+++ 344-reverse-string/reverse-string.ts
@@ -0,0 +1,15 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    let l: number =0;
+    let r: number =s.length-1;
+
+    while(l<r){
+        let temp = s[r];
+        s[r]=  s[l]
+        s[l] = temp
+        r--;
+        l++;
+    }
+};
