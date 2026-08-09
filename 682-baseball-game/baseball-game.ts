1fa86577b028fa62519f53c401b5c1b10dfd6df2
Asmaa <aasmaaelkady@gmail.com>
2026-08-10 02:08:08 +0300

    Time: 0 ms (100.00%) | Memory: 54.6 MB (94.71%) - LeetSync

--- 682-baseball-game/baseball-game.ts
+++ 682-baseball-game/baseball-game.ts
@@ -1,17 +1,21 @@
 function calPoints(operations: string[]): number {
-    let res : number [] = []
+    let res : number [] = [];
+    let sum : number =0;
 
     for(let i=0; i<operations.length; i++){
-        console.log(res)
         if(operations[i] == "C"){
-            res.pop()
+            let val = res.pop();
+            sum-=val
         }else if(operations[i] == 'D'){
             res.push(res[res.length-1]*2)
+            sum+= res[res.length-1]
         }else if(operations[i] == '+'){
             res.push(res[res.length-1]+res[res.length-2])
-        } else{
+            sum+= res[res.length-1]
+        }else{
             res.push(Number(operations[i]))
+            sum+= Number(operations[i])
         }
     }
-    return res.reduce((total,num)=> total+num, 0)
+    return sum
 };
